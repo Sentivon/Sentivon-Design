8941c1b4ade7ebfc49b1efcff43803875a5484af
Nichtmetall <hofmannanton@outlook.de>
2023-03-14 15:09:04 +0100

    Init

---
+++ sentivon-design/README.md
@@ -0,0 +1,1 @@
+# Sentivon Design System
---
+++ sentivon-design/src/button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../global.css';
+
+const Button = () => {
+  return <button className='bg-slate-200'>Test</button>;
+};
+
+export default Button;
